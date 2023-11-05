86407d8213f2681df1795ede0dce1be87b07c26a
Magoninho <magoninhogamer@gmail.com>
2023-11-05 18:42:18 -0300

    new menu

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -8,5 +8,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$ParallaxBackground.scroll_base_offset += Vector2(-80, 0) * delta
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().change_scene_to_file("res://Scenes/DifficultySelector.tscn")
